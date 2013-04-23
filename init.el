3b07cadb878e4617a5e51e4029fb9feb940c05f0
Marius Cobzarenco <marius.cobzarenco@gmail.com>
2013-04-23 17:27:51 +0100

    increased width of sr-speedbar

--- init.el
+++ init.el
@@ -8,9 +8,9 @@
 
 (require 'sr-speedbar)
 (global-set-key (kbd "s-s") 'sr-speedbar-toggle)
-(setq sr-speedbar-width-x 30)
-(setq sr-speedbar-console 30)
 (setq sr-speedbar-right-side nil)
+(setq sr-speedbar-width 30)
+(setq sr-speedbar-console 30)
 
 
 ;; Hidding Hidden Files in Emacs dired (M-o to toggle)
